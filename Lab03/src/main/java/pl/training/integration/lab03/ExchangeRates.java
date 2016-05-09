b3a6fb8dc2af25fee0f0ba5e82c178f20621a86d
Lukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2016-05-09 16:11:36 +0200

    Spring integration

--- Lab03/src/main/java/pl/training/integration/lab03/ExchangeRates.java
+++ Lab03/src/main/java/pl/training/integration/lab03/ExchangeRates.java
@@ -2,6 +2,7 @@ package pl.training.integration.lab03;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @XmlRootElement(name = "tabela_kursow")
 public class ExchangeRates {
 
+    @XmlElement(name = "pozycja")
     private List<Entry> entries;
 
     public List<Entry> getEntries() {
