afdf46cf70ade7c5c2c9e37c988ae2dbde33d2ff
Lukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2016-05-10 10:17:49 +0200

    Spring integration

---
+++ Lab04/src/main/java/pl/training/integration/lab04/App.java
@@ -0,0 +1,16 @@
+package pl.training.integration.lab04;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    private static final int SLEEP_TIME = 20_000;
+    private static final long REQUEST_ID = 1;
+
+    public static void main(String[] args) throws InterruptedException {
+        try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml", "integration.xml")) {
+            Thread.sleep(SLEEP_TIME);
+        }
+    }
+
+}
